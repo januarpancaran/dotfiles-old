a0341a570ef063384db6d1db3a669e4c298ad57e
januarpancaran <januar352@gmail.com>
2025-01-10 05:50:55 +0700

    adding kitty dots

--- install.sh
+++ install.sh
@@ -19,17 +19,17 @@ else
 fi
 
 echo "Installing Packages..."
-$AUR_HELPER -S base-devel bluez bluez-utils networkmanager hyprland foot nautilus polkit-gnome brightnessctl pipewire wireplumber waybar swaync hypridle hyprlock rofi-wayland swww wlogout qt5-wayland qt6-wayland xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xwaylandvideobridge grim slurp wl-clipboard htop trash-cli alsa-utils alsa-firmware pipewire-pulse pipewire-alsa blueberry xorg-xhost bat fastfetch eog network-manager-applet unzip unrar wget openssh xdg-desktop-portal libnotify zoxide playerctl gruvbox-gtk-theme-git --needed --noconfirm
+$AUR_HELPER -S base-devel bluez bluez-utils networkmanager hyprland kitty polkit-gnome brightnessctl pipewire wireplumber waybar swaync hypridle hyprlock rofi-wayland swww wlogout qt5-wayland qt6-wayland xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xwaylandvideobridge grim slurp wl-clipboard htop trash-cli alsa-utils alsa-firmware pipewire-pulse pipewire-alsa blueberry xorg-xhost bat fastfetch eog network-manager-applet unzip unrar wget openssh xdg-desktop-portal libnotify zoxide playerctl gruvbox-gtk-theme-git fzf ripgrep --needed --noconfirm
 
 echo "Installing Fonts..."
 $AUR_HELPER -S ttf-meslo-nerd ttf-jetbrains-mono-nerd ttf-space-mono-nerd otf-font-awesome ttf-material-symbols-variable-git noto-fonts-emoji noto-fonts-cjk terminus-font --noconfirm 
 
 echo "Installing Applications..."
-$AUR_HELPER -S auto-cpufreq timeshift-autosnap firefox visual-studio-code-bin neovim spotify-launcher obs-studio telegram-desktop mpv webcord --needed --noconfirm 
+$AUR_HELPER -S auto-cpufreq timeshift-autosnap firefox neovim spotify-launcher obs-studio telegram-desktop mpv webcord --needed --noconfirm 
 
 # Enabling Services
 sudo systemctl enable --now NetworkManager bluetooth auto-cpufreq cronie
-systemctl enable --user --now pipewire.socket pipewire.service foot-server.service foot-server.socket
+systemctl enable --user --now pipewire.socket pipewire.service 
 
 # Moving all the dotfiles
 if [ ! -d "$HOME/.config" ]; then
@@ -54,3 +54,13 @@ for folder in "$SRC"*; do
                 cp -r "$item" "$destination_folder"
         done
 done
+
+GRUVBOX_DIR="/usr/share/themes/Gruvbox-Dark"
+if [ -d "$GRUVBOX_DIR" ]; then
+	mkdir -p "$HOME/gtk-4.0"
+	ln -s "$GRUVBOX_DIR/gtk-4.0/assets" "$HOME/gtk-4.0/assets"
+	ln -s "$GRUVBOX_DIR/gtk-4.0/gtk.css" "$HOME/gtk-4.0/gtk.css"
+	ln -s "$GRUVBOX_DIR/gtk-4.0/gtk-dark.css" "$HOME/gtk-4.0/gtk-dark.css"
+else
+	echo "Gruvbox theme not installed in your system"
+fi
