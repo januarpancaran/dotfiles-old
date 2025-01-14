6557c3cec51c5f32e3056b95c1ea10429c8dd7ca
januarpancaran <januar352@gmail.com>
2025-01-14 10:35:45 +0700

    fixing typo in neovim plugins

--- Configs/nvim/lua/plugins.lua
+++ Configs/nvim/lua/plugins.lua
@@ -40,7 +40,7 @@ return {
 	{
 		'hrsh7th/nvim-cmp',
 		dependencies = {
-			'L3M0N4D3/LuaSnip',
+			'L3MON4D3/LuaSnip',
 			'saadparwaiz1/cmp_luasnip',
 			'rafamadriz/friendly-snippets',
 			'hrsh7th/cmp-nvim-lsp',
