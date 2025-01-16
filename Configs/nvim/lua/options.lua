36ad332d3c7fc980c1b409abc8e0d462057b94c2
januarpancaran <januar352@gmail.com>
2025-01-16 16:07:04 +0700

    changing default tab to 2

--- Configs/nvim/lua/options.lua
+++ Configs/nvim/lua/options.lua
@@ -6,9 +6,9 @@ vim.o.relativenumber = true
 
 vim.o.signcolumn = "yes"
 
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
 
 vim.o.updatetime = 300
 
